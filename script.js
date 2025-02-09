a4e529ed9d9daa9db8da359c08d9f40623ee470e
Iker Vélez <168359086+Iker1211@users.noreply.github.com>
2025-02-09 03:47:06 +0000

    Add functions to change button textContent according to max-width

--- script.js
+++ script.js
@@ -10,3 +10,40 @@ if (0 <= hour && hour < 12 ) {
 } else {
     greetings.textContent = 'Good Night';
 }
+
+let btn = document.querySelector('button');
+var medium_width = window.matchMedia("(max-width: 1330px)");
+
+function less_arrows(x) {
+    if (x.matches) { 
+    btn.textContent = '➡️➡️➡️➡️➡️➡️➡️Send your message⬅️⬅️⬅️⬅️⬅️⬅️⬅️';
+    } else {
+    btn.textContent = '➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️➡️Send your message⬅️⬅️⬅️⬅️⬅️⬅️⬅️⬅️⬅️⬅️⬅️⬅️';
+    }
+}
+
+less_arrows(medium_width);
+
+medium_width.addEventListener("change", function() {
+less_arrows(medium_width);
+});
+
+btn.addEventListener('click', (event) => {
+
+    btn.style.height = '3rem';
+    btn.style.fontSize = 'clamp(1rem, 4vw, 1.25rem )';
+
+    function less_ballons(x) {
+        if (x.matches) { 
+        btn.textContent = '🎈🎈🎈🎈🎈🎈🦚🎈🎈🎈🎈🎈🎈';
+        } else {
+        btn.textContent = '🎈🎈🎈🎈🎈🎈🎈🎈🎈🦚🎈🎈🎈🎈🎈🎈🎈🎈🎈';
+        }
+    }
+
+    less_ballons(medium_width);
+
+    medium_width.addEventListener("change", function() {
+    less_ballons(medium_width);
+    });
+});
